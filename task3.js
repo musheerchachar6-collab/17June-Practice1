9db8a7ce2bf28e53d140e46abb5d673978b35a2c
musheerchachar <Musheerchachar6@gmail.com>
2026-06-17 16:55:21 +0500

    task3 is done

---
+++ task3.js
@@ -0,0 +1,21 @@
+let grade=prompt("Enter Your Grade ")
+switch(grade)
+{
+    case 'A':
+        console.log("Excellent")
+        break;
+    case 'B':
+        console.log("Very Good ")
+        break;
+    case 'C':
+        console.log("Good")
+        break;
+    case 'D':
+        console.log(" Needs Improvement")
+        break;
+    case 'E':
+        console.log(" Failed")
+        break;
+    default:
+        console.log("Invaild Grade")
+}
