4fe5bc1948b6631ec8ba43787406cfed6254bd54
musheerchachar <Musheerchachar6@gmail.com>
2026-06-17 17:03:50 +0500

    task4 is done

---
+++ task4.js
@@ -0,0 +1,14 @@
+let trafic=prompt("Enter a trafic Lighit Colors i.e Red,yellow ,Green")
+switch(trafic){
+    case "Red":
+        console.log("stop")
+        break;
+    case "Yellow":
+        console.log(" get Ready")
+        break;
+    case "Red":
+        console.log("Go")
+        break;
+    default:
+        console.log("Invaild Color")
+}
