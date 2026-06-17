9b21ebf087f3c208223ea34b82424b11a65c04c2
Musheerchachar <Musheerchachar6@gmail.com>
2026-06-17 15:41:49 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+let month=prompt("Enter number 1 to 12")
+month=Number(month)
+switch(month){
+    case 1:
+        console.log("January")
+        break;
+    case 2:
+        console.log("febuary ")
+        break;
+    case 3:
+        console.log("March")
+        break;
+    case 4:
+        console.log("April")
+        break;
+    case 5:
+        console.log("may")
+        break;
+    case 6:
+        console.log("June")
+        break;
+    case 7:
+        console.log("Julay")
+        break;
+    case 8:
+        console.log("August")
+        break;
+    case 9:
+        console.log("Septomber")
+        break;
+    case 10:
+        console.log("Octomber")
+        break;
+    case 11:
+        console.log("Novber")
+        break;
+    case 12:
+        console.log("December")
+        break;
+    default:
+        console.log("Invaild")
+}
